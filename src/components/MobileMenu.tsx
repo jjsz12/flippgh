8e04fbaeb92028320e91b25b37ae3c4c5629c992
Jared Schmidt (jjsz12) <jjsz116a@gmail.com>
2022-02-23 22:32:02 -0500

    updated mobile nav to have dropdown

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,11 +1,75 @@
-import React from "react";
-import { Menu } from "semantic-ui-react";
-import MenuItems from "./MenuItems";
+import { useNavigate } from "react-router-dom";
+import { Dropdown, Image, Menu } from "semantic-ui-react";
+import logo from "../assets/flip-pgh-logo-white.svg";
+import { useWindowSize } from "../hooks/useWindowSize";
 
 function MobileMenu() {
+  const navigate = useNavigate();
+  const size = useWindowSize();
+
+  const renderLogo = () => {
+    if (size.width && size.width > 640) {
+      return <Image src={logo} />;
+    }
+    return <Image src={logo} style={{ height: 50, width: 50 }} />;
+  };
+
   return (
-    <Menu fixed="top" inverted style={{ overflow: "auto" }}>
-      <MenuItems />
+    <Menu fixed="top" inverted style={{ display: "flex" }}>
+      <Menu.Item
+        name="logo"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        {renderLogo()}
+      </Menu.Item>
+      <Dropdown
+        item
+        text="Menu"
+        icon="bars"
+        simple
+        style={{ "margin-left": "auto" }}
+        className="menu-dropdown"
+      >
+        <Dropdown.Menu style={{ right: 0, left: "auto" }}>
+          <Dropdown.Item
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Home
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              navigate("/schedule");
+            }}
+          >
+            Schedule
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              navigate("/locations");
+            }}
+          >
+            Location Info
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              navigate("/rules");
+            }}
+          >
+            Format & Rules
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              navigate("/results");
+            }}
+          >
+            Results & Stats
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </Menu>
   );
 }
