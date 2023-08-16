a8b21f6a67ee9a06b65e6bd6b463d7852acc3907
Jared Schmidt (jjsz12) <jjsz116a@gmail.com>
2023-08-16 10:41:06 -0400

    update spl data; show finals links

--- src/pages/superflip/SuperflipMatchplayLinksView.tsx
+++ src/pages/superflip/SuperflipMatchplayLinksView.tsx
@@ -1,29 +1,29 @@
-import { Grid } from "semantic-ui-react";
+// import { Grid } from "semantic-ui-react";
 import ContentContainer from "../../components/ContentContainer";
-import { SPLMatchplayLinks } from "./components/SPLMatchplayLinks";
+// import { SPLMatchplayLinks } from "./components/SPLMatchplayLinks";
 import { SPLPageHeader } from "./components/SPLPageHeader";
 
 // import data from "../../common/data/spl/spl_series_data_2626.json";
-import data from "../../common/data/spl/spl_series_data_2801.json";
-import { getTournamentIdTuples } from "./utils";
-// import { SPLMatchplayFinalsLinks } from "./components/SPLMatchplayFinalsLinks";
+// import data from "../../common/data/spl/spl_series_data_2801.json";
+// import { getTournamentIdTuples } from "./utils";
+import { SPLMatchplayFinalsLinks } from "./components/SPLMatchplayFinalsLinks";
 
-const tournament_ids = getTournamentIdTuples(data.data.tournamentIds);
+// const tournament_ids = getTournamentIdTuples(data.data.tournamentIds);
 
-const weektoDateMap: { [key: number]: string } = {
-  0: "Monday 7/10",
-  1: "Monday 7/17",
-  2: "Monday 7/24",
-  3: "Monday 7/31",
-  4: "Monday 8/7",
-  5: "Monday 8/14",
-};
+// const weektoDateMap: { [key: number]: string } = {
+//   0: "Monday 7/10",
+//   1: "Monday 7/17",
+//   2: "Monday 7/24",
+//   3: "Monday 7/31",
+//   4: "Monday 8/7",
+//   5: "Monday 8/14",
+// };
 
 export const SuperflipMatchplayLinksView = () => {
   return (
     <ContentContainer>
       <SPLPageHeader />
-      <h3>Matchplay Links (Summer 2023 Season)</h3>
+      {/* <h3>Matchplay Links (Summer 2023 Season)</h3>
       <Grid stackable columns={3}>
         {Object.keys(weektoDateMap).map((key) => {
           const idTuple = tournament_ids[Number(key)];
@@ -45,8 +45,8 @@ export const SuperflipMatchplayLinksView = () => {
             </Grid.Column>
           );
         })}
-      </Grid>
-      {/* <SPLMatchplayFinalsLinks /> */}
+      </Grid> */}
+      <SPLMatchplayFinalsLinks />
     </ContentContainer>
   );
 };
--- src/pages/superflip/components/SPLMatchplayFinalsLinks.tsx
+++ src/pages/superflip/components/SPLMatchplayFinalsLinks.tsx
@@ -1,8 +1,8 @@
 import MatchplayButton from "../../../components/MatchplayButton";
 
-const aDivLink = "https://next.matchplay.events/tournaments/103751";
-const bDivLink = "https://next.matchplay.events/tournaments/103753";
-const xDivLink = "https://next.matchplay.events/tournaments/103754";
+const aDivLink = "https://next.matchplay.events/tournaments/111924";
+const bDivLink = "https://next.matchplay.events/tournaments/111925";
+// const xDivLink = "https://next.matchplay.events/tournaments/103754";
 
 export const SPLMatchplayFinalsLinks = () => {
   return (
@@ -15,7 +15,7 @@ export const SPLMatchplayFinalsLinks = () => {
           fontWeight: "bold",
         }}
       >
-        SPL Spring 2023 Finals Links
+        SPL Summer 2023 Finals Links
       </div>
       <div
         style={{
@@ -37,7 +37,7 @@ export const SPLMatchplayFinalsLinks = () => {
         <span style={{ marginRight: "2rem" }}>B Division:</span>
         <MatchplayButton link={bDivLink} primary compact />
       </div>
-      <div
+      {/* <div
         style={{
           display: "flex",
           justifyContent: "center",
@@ -46,7 +46,7 @@ export const SPLMatchplayFinalsLinks = () => {
       >
         <span style={{ marginRight: "2rem" }}>X Division:</span>
         <MatchplayButton link={xDivLink} primary compact />
-      </div>
+      </div> */}
     </>
   );
 };
